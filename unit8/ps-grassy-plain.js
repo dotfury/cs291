68cb7ced358c15a3c297f3a571fc9b68b2cb9954
dotfury <cm2jr4@gmail.com>
2026-06-06 17:01:37 +0900

    Texture exercises - 2026/06/05

--- unit8/ps-grassy-plain.js
+++ unit8/ps-grassy-plain.js
@@ -25,9 +25,13 @@ function fillScene() {
 
 	// GROUND
 	// Student: texture is located at URL /media/img/cs291/textures/grass512x512.jpg
+  const texture = THREE.ImageUtils.loadTexture('/media/img/cs291/textures/grass512x512.jpg');
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.repeat.set(10, 10);
 	var solidGround = new THREE.Mesh(
 		new THREE.PlaneGeometry( 10000, 10000, 100, 100 ),
-		new THREE.MeshLambertMaterial( { color: 0xFFFFFF } ) );
+		new THREE.MeshLambertMaterial( { map: texture } ) );
 	solidGround.rotation.x = - Math.PI / 2;
 
 	scene.add( solidGround );
